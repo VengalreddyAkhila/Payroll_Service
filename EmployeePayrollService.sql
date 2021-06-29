704ada702f50dc10221c4ed2854f5214741d910a
VengalreddyAkhila <akhila.it1801@gmail.com>
2021-06-29 21:44:12 +0530

    [akhila]Refactor:Extend table with basic pay

--- EmployeePayrollService.sql
+++ EmployeePayrollService.sql
@@ -1,11 +1,11 @@
 
-//..UC-1:-create a payroll service database
+--UC-1:-create a payroll service database
 
 create database EmployeePayroll
 
 use EmployeePayroll
 
-//..UC-2:-create payrollservice table
+--UC-2:-create payrollservice table
 
 create Table EmployeePayroll(
 EmployeeID int Identity(1,1) primary key,
@@ -14,7 +14,7 @@ Salary float,
 StartDate date
 )
 
-//..UC-3:-CRUD operation
+--UC-3:-CRUD operation
 
 Insert into EmployeePayroll(EmployeeName,Salary,StartDate)
 VALUES ('Akhila', 80000.00 , '2020-09-12'),
@@ -24,16 +24,16 @@ VALUES ('Akhila', 80000.00 , '2020-09-12'),
 		('Prasad',40000.00, '2021-06-24'),
 		('Radhika',35000.00, '2021-07-20');
 
-//..UC-4:- Retrieve all data from employeeparoll table
+--UC-4:- Retrieve all data from employeeparoll table
 
 select *from EmployeePayroll
 
-//..UC5:-particular employee as well as all employees who have joined in particular date range 
+--UC5:-particular employee as well as all employees who have joined in particular date range 
 
 Select Salary from EmployeePayroll Where EmployeeName='Anuhya'
 Select Salary from EmployeePayroll Where StartDate BETWEEN CAST('2021-07-08' as date) and GetDate();
 
-//..UC6:- Add gender to employeetable and correct employee gender
+--UC6:- Add gender to employeetable and correct employee gender
 
 Alter Table EmployeePayroll
 Add Gender varchar(32);
@@ -45,7 +45,7 @@ UPDATE EmployeePayroll set Gender ='Male' where EmployeeName='Venu'
 UPDATE EmployeePayroll set Gender ='male' where EmployeeName='Prasad'
 UPDATE EmployeePayroll set Gender ='Female' where EmployeeName='Radhika'
 
-//--UC7:- Use of Sum, Avg, Min, Max, Count Functions
+--UC7:- Use of Sum, Avg, Min, Max, Count Functions
 
 Select SUM(Salary) from EmployeePayroll
 Select AVG(Salary) from EmployeePayroll
@@ -58,11 +58,20 @@ Select AVG(Salary) from EmployeePayroll where Gender = 'Female' GROUP BY Gender;
 Select AVG(Salary) from EmployeePayroll where Gender = 'Male' GROUP BY Gender;
 
 
-//--Section 2:-(ER Diagram)
-//--UC8-Extend the EmployeePayroll table with Phone, address and department
+--Section 2:-(ER Diagram)
+--UC8:-Extend the EmployeePayroll table with Phone, address and department
+
 Alter Table EmployeePayroll Add
 PhoneNumber varchar(10),
 Department varchar(30),
 Address varchar(30), 
 CONSTRAINT DF_Address DEFAULT 'Indian' FOR Address
 
+--UC9:-extend employee table with basic pay
+
+Alter Table EmployeePayroll Add
+BasicPay float,
+Deduction float,
+TaxablePay float,
+IncomeTax float,
+NetPay float;
