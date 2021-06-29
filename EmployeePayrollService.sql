e1edf6a374d7729f418cdd07f6eaa8fe4cb29a05
VengalreddyAkhila <akhila.it1801@gmail.com>
2021-06-29 20:28:27 +0530

    [akhila]Refactor:insert employeepayroll details

--- EmployeePayrollService.sql
+++ EmployeePayrollService.sql
@@ -5,7 +5,7 @@ create database EmployeePayroll
 
 use EmployeePayroll
 
-UC2-2:-create payrollservice table
+UC-2:-create payrollservice table
 
 create Table EmployeePayroll(
 EmployeeID int Identity(1,1) primary key,
@@ -13,3 +13,13 @@ EmployeeName varchar(32),
 Salary float,
 StartDate date
 )
+
+UC-3:-CRUD operation
+
+Insert into EmployeePayroll(EmployeeName,Salary,StartDate)
+VALUES ('Akhila', 80000.00 , '2020-09-12'),
+        ('Anuhya',75000.00, '2020-07-22'),
+		('Ramu Sham',55000.00, '2021-08-26'),
+		('Venu',45000.00, '2021-05-08'),
+		('Prasad',40000.00, '2021-06-24'),
+		('Radhika',35000.00, '2021-07-20');
