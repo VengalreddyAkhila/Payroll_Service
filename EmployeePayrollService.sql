ec30b513fa41e51cd6638f49c4b85f5f9d360dc9
VengalreddyAkhila <akhila.it1801@gmail.com>
2021-06-29 20:18:30 +0530

    [akhila]Refactor:CreateTable

--- EmployeePayrollService.sql
+++ EmployeePayrollService.sql
@@ -4,3 +4,12 @@ UC-1:-create a payroll service database
 create database EmployeePayroll
 
 use EmployeePayroll
+
+UC2-2:-create payrollservice table
+
+create Table EmployeePayroll(
+EmployeeID int Identity(1,1) primary key,
+EmployeeName varchar(32),
+Salary float,
+StartDate date
+)
